18188872d2d24b3d73580660aaf49405b57d77dc
wsgauri <gandhalebhausaheb9@gmail.com>
2024-10-18 13:35:02 +0530

    Add files via upload

---
+++ comRepoAddinfo.java
@@ -0,0 +1,10 @@
+package com.project.repositry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.Model.AddBus;
+
+public interface comRepoAddinfo extends JpaRepository<AddBus, Integer>
+{
+
+}
